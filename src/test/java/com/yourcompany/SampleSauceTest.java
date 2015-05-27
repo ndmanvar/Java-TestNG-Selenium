a9c9b79c03e58188b6ed95d278f9110db8f10286
Neil <neil@Neils-MacBook-Pro.local>
2015-05-26 18:10:50 -0700

    Only using one browser

--- src/test/java/com/yourcompany/SampleSauceTest.java
+++ src/test/java/com/yourcompany/SampleSauceTest.java
@@ -68,11 +68,15 @@ public class SampleSauceTest implements SauceOnDemandSessionIdProvider, SauceOnD
     @DataProvider(name = "hardCodedBrowsers", parallel = true)
     public static Object[][] sauceBrowserDataProvider(Method testMethod) {
         return new Object[][]{
-                new Object[]{"internet explorer", "11", "Windows 8.1"},
+                // new Object[]{"internet explorer", "11", "Windows 8.1"},
+                // new Object[]{"chrome", "41", "Windows XP"},
+                // new Object[]{"safari", "7", "OS X 10.9"},
+                // new Object[]{"firefox", "36", "Windows 7"},
+                // new Object[]{"firefox", "35", "Windows 7"}
                 new Object[]{"chrome", "41", "Windows XP"},
-                new Object[]{"safari", "7", "OS X 10.9"},
-                new Object[]{"firefox", "36", "Windows 7"},
-                new Object[]{"firefox", "35", "Windows 7"}
+                new Object[]{"chrome", "41", "Windows XP"},
+                new Object[]{"chrome", "41", "Windows XP"}
+
         };
     }
 
